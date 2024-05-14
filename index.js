9d6ff1a6ec10a7e9a5bef71856b319b114a19e0b
Vladyslav <vladarius23@gmail.com>
2024-05-14 15:16:20 +0200

    Final release

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 var cors = require('cors');
 
-//Load environment file
 require('dotenv').config();
-
-//Load database config
 require('./config/db');
 
 const apiRoutes = require('./routes/Api');
@@ -12,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//API routes
 app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
